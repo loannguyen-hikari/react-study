e296ef4ae53baa9ea1c22e79241b51d551e1985e
loannguyen <nguyenloandhnn96@gmail.com>
2024-09-08 10:30:23 +0700

    study react component

---
+++ src/components/MyComponent.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+class MyComponent extends Component {
+  render(){
+    return (
+      <div>My first component</div>
+    );
+  }
+}
+
+export default MyComponent
